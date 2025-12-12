5faccc8a9ef921c8f48cac23ddcb62d2dd6cca40
Ayush <cyber.devil.979@gmail.com>
2025-12-12 18:42:55 +0530

    Improve input handling and add comments
    Refactor input handling and comments for clarity.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-a = int(input("No. of items needed to stored: "))
+a = int(input("No. of items needed to stored: ")) #take input that how many items are needed
 
 lis = []
 
 n = 1
 
-while(a>0) :
-   str = input(f"Item {n}: ") 
+while(a>0) :    #by while loop instead of 
+   str = input(f"Item {n}: ")    #create a loop to take input accoring to a
    lis.append(str)
    a = a-1 
    n = n + 1
 
-lis.sort()
+lis.sort() #sort list alphabeticaly ( a ,b ,c)
 print(lis)
 
 input("Press enter to exit....")
+
